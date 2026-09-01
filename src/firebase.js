a551128f2602e3b5184554d44031642cb4b2045e
lucianodilascio <luciano.dilascio14@gmail.com>
2026-09-01 14:31:47 -0300

    Ocultado de credenciales de firebase en  variables de entorno

--- src/firebase.js
+++ src/firebase.js
@@ -4,12 +4,12 @@ import { getAuth } from "firebase/auth";
 
 // credenciales únicas del proyecto
 const firebaseConfig = {
-  apiKey: "AIzaSyCgvPA-3pxdqZjudkWA-0mkg6TYGXSDRhQ",
-  authDomain: "gym-manager-57be7.firebaseapp.com",
-  projectId: "gym-manager-57be7",
-  storageBucket: "gym-manager-57be7.firebasestorage.app",
-  messagingSenderId: "7847335667",
-  appId: "1:7847335667:web:35bc999cc535852b9a8986"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID
 };
 
 // Inicializamos la aplicación
